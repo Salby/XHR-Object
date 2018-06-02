23b98c245babf69327826e566dd1cc7e63218bb2
Sander Larsen <srdlarsen@gmail.com>
2018-06-02 13:46:34 +0200

    Update xhr.js

--- xhr.js
+++ xhr.js
@@ -22,41 +22,41 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-var xhr = {
+'use strict';
 
+var xhr = {
   /**
    * Sends data via GET
    * 
    * @param {url} first Url you want to send to, including data
-   * @param {success} second Lets you work with the response in a function
+   * @param {success} second Lets you work with response in a function
    * @returns {request}
    */
-  get: function(url, success) {
-    var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
-    xhr.open('GET', url);
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState > 3 && xhr.status === 200) success(xhr.responseText);
+  get: function get(url, success) {
+    var request = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+    request.open('GET', url);
+    request.onreadystatechange = function () {
+      if (request.readyState > 3 && request.status === 200) success(request.responseText);
     };
-    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-    xhr.send();
-    return xhr;
+    request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    request.send();
+    return request;
   },
-
   /**
-   * Sends data via POST
+   * Send data via POST
    * 
-   * @param {url} first Url you wish to send data to
-   * @param {data} second Data you wish to send. Takes a string or an object
-   * @param {success} third Lets you work with the response in a function
+   * @param {url} first Url you want to send data to
+   * @param {data} second Data you want to send, takes string or object
+   * @param {success} third Lets you work with response in a function
    * @returns {request}
    */
-  post: function(url, data, success) {
-    var params = typeof data == 'string' ? data : Object.keys(data).map(
-      function(k) { return encodeURIComponent(k) + '=' + encodeURIComponent(data[k]) }
-    ).join('&');
+  post: function post(url, data, success) {
+    var params = typeof data == 'string' ? data : Object.keys(data).map(function (k) {
+      return encodeURIComponent(k) + '=' + encodeURIComponent(data[k]);
+    }).join('&');
     var request = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
     request.open('POST', url);
-    request.onreadystatechange = function() {
+    request.onreadystatechange = function () {
       if (request.readyState > 3 && request.status === 200) success(request.responseText);
     };
     request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
